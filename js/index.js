da0b59ca1e179d266590752e99509a442f512d9a
felixhaeberle <kontakt@felixhaeberle.de>
2020-06-01 15:20:18 +0200

    add first prototype stage

---
+++ js/index.js
@@ -0,0 +1,3 @@
+SVGInject.setOptions({
+  makeIdsUnique: false
+});
---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('#toggle-info1').on('click', function(){
+    $('#info1').toggle();
+  });
+  $('#toggle-info2').on('click', function(){
+    $('#info2').toggle();
+  });
+  $('#toggle-info3').on('click', function(){
+    $('#info3').toggle();
+  });
+});
